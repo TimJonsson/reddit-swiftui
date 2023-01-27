7e972a6078f376f053c0f8e2422f3cb9575b4a7c
TimJonsson <tim.jonsson96@hotmail.com>
2023-01-27 08:42:19 +0100

    Update FilterScreen.swift
    Removed commented code

--- RedditUITests/ScreenObjects/FilterScreen.swift
+++ RedditUITests/ScreenObjects/FilterScreen.swift
@@ -19,7 +19,6 @@ class FilterScreen {
 
     @discardableResult
     func chooseSortMethod(sortingMethod: String) -> Self {
-//        let fistButton = app.buttons.matching(identifier: "\(sortingMethod)").element(boundBy: 1)
         app.buttons["\(sortingMethod)"].firstMatch.tapElement()
         return self
     }
